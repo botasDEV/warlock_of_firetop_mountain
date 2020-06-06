d6304638b8c7bd250f558df67252a1905a95f7c7
Rafael Botas <rafael.botas@future-healthcare.eu>
2020-06-06 03:51:33 +0100

    Created Player FPS

---
+++ Projeto_Fase0/Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public float speed = 4f;
+    public float gravity = -2f;
+    public float jumpHeight = 2f;
+    public LayerMask groundMask;
+    Vector3 velocity;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsGrounded() && velocity.y < 0)
+        {
+            velocity.y = 0f;
+        }
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(movement * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+    }
+
+}
---
+++ Projeto_Fase0/Assets/PlayerVision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVision : MonoBehaviour
+{
+    public float sensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveSpeed = sensitivity * Time.deltaTime;
+        float moveX = Input.GetAxis("Mouse X") * moveSpeed;
+        float moveY = Input.GetAxis("Mouse Y") * moveSpeed;
+
+        xRotation -= moveY;
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * moveX);
+    }
+}
